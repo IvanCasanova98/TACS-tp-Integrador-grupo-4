44ab228dc88eea834dde782d926e208a355531e4
JulietaAbuin <julieta.abuin@despegar.com>
2021-04-18 21:27:35 -0300

    fix routing

--- src/main/scala/routes/Routes.scala
+++ src/main/scala/routes/Routes.scala
@@ -8,64 +8,64 @@ import akka.http.scaladsl.server.Directives._
 object Routes {
 
   def apply(): Route = {
-    path("asd") {
-      get {
-        complete(200, "Happy christmas")
+    concat(
+      path("asd") {
+        get {
+          complete(200, "Happy christmas")
+        }
       }
-    }
-    path("login") {
-      post {
-        complete("")
+        ~ path("login") {
+        post {
+          complete("")
+        }
       }
-    }
-    pathPrefix("decks") {
-      post {
-        //BODY: name, card_ids []
-        complete(201, "deck created with id")
-      }
-      path(IntNumber) { deckId =>
-        put {
+        ~ pathPrefix("decks") {
+        post {
           //BODY: name, card_ids []
-          complete(204, s"$deckId")
+          complete(201, "deck created with id")
         }
-        delete {
-          complete(204, s"Deck deleted $deckId")
-        }
-      }
-    }
-
-    pathPrefix("matches") {
-      post {
-        //BODY deck_id, user_ids [], status CREATED
-        complete(201, "Match created")
-      }
-      path(IntNumber / "result") { matchId =>
-        get {
-          complete(200, s"$matchId result: user1 won")
+        path(IntNumber) { deckId =>
+          put {
+            //BODY: name, card_ids []
+            complete(204, s"$deckId")
+          }
+          delete {
+            complete(204, s"Deck deleted $deckId")
+          }
         }
       }
-      path(IntNumber / "movements") { matchId =>
-        get {
-          complete(200, s"Match $matchId Movements []: attribute, cards, result")
-        }
-      }
-      path(IntNumber / "status") { matchId =>
-        patch {
-          //BODY status = { FINISHED | IN_PROCESS | PAUSED | CANCELED}
-          complete(204, "Match finished")
-        }
-      }
-      parameters("user_id".as[String]) { (userId) =>
-        complete(200, "")
-      }
-    }
-
-    pathPrefix("statistics") {
+        ~ pathPrefix("statistics") {
         parameters("search_by".as[String], "user_id".optional) { (searchBy, userId) =>
           //Query params search match or user
-           complete(200, "")
+          complete(200, "")
         }
       }
-    }
+        ~ pathPrefix("matches") {
+        post {
+          //BODY deck_id, user_ids [], status CREATED
+          complete(201, "Match created")
+        }
+        path(IntNumber / "result") { matchId =>
+          get {
+            complete(200, s"$matchId result: user1 won")
+          }
+        }
+        path(IntNumber / "movements") { matchId =>
+          get {
+            complete(200, s"Match $matchId Movements []: attribute, cards, result")
+          }
+        }
+        path(IntNumber / "status") { matchId =>
+          patch {
+            //BODY status = { FINISHED | IN_PROCESS | PAUSED | CANCELED}
+            complete(204, "Match finished")
+          }
+        }
+        parameters("user_id".as[String]) { (userId) =>
+          complete(200, "")
+        }
+      }
+    )
+  }
 }
 
--- src/main/scala/server/HttpServer.scala
+++ src/main/scala/server/HttpServer.scala
@@ -3,8 +3,6 @@ package server
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
 import routes.Routes
 
 import scala.concurrent.ExecutionContextExecutor
