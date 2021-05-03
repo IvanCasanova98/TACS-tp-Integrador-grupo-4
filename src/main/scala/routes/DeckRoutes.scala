b05b1a647e0afec75f8c88078af8186fcccc97ba
JulietaAbuin <julieta.abuin@despegar.com>
2021-05-03 19:01:48 -0300

    change routes

--- src/main/scala/routes/DeckRoutes.scala
+++ src/main/scala/routes/DeckRoutes.scala
@@ -2,9 +2,9 @@ package routes
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
-import org.json4s.{DefaultFormats, Formats}
+import akka.http.scaladsl.server.{RejectionHandler, Route}
 import org.slf4j.{Logger, LoggerFactory}
+import routes.Routes.{cors, settings}
 import routes.Utils.handleRequest
 import routes.inputs.DeckInputs.PartialDeckInput
 import serializers.Json4sSnakeCaseSupport
@@ -15,37 +15,48 @@ object DeckRoutes extends Json4sSnakeCaseSupport {
   val logger: Logger = LoggerFactory.getLogger(classOf[DeckService])
 
   def apply(deckService: DeckService): Route = {
-    concat(
-      path("decks") {
-        get {
-          logger.info("[GET] /decks")
-          complete(StatusCodes.OK, deckService.getAll)
-        }
-      },
-      path("decks") {
-        post {
-          entity(as[PartialDeckInput]) { deck =>
-            logger.info("[POST] /decks")
-            val deckId: Int = deckService.createDeck(deck)
-            handleRequest(() => s"Deck created with id: $deckId", StatusCodes.Created)
+    cors(settings) {
+      concat(
+        path("decks") {
+          get {
+            logger.info("[GET] /decks")
+            complete(StatusCodes.OK, deckService.getAll)
           }
         }
-      },
-      path("decks" / IntNumber) { deckId =>
-        put {
-          entity(as[PartialDeckInput]) { deck =>
-            logger.info(s"[PUT] /decks/$deckId")
-            handleRequest(() => deckService.updateDeck(deckId, deck))
+        ,
+        path("decks") {
+          handleRejections(RejectionHandler.default) {
+            post {
+              entity(as[PartialDeckInput]) { deck =>
+                logger.info("[POST] /decks")
+                val deckId: Int = deckService.createDeck(deck)
+                handleRequest(() => s"Deck created with id: $deckId", StatusCodes.Created)
+              }
+            }
           }
         }
-      },
-      path("decks" / IntNumber) { deckId =>
-        delete {
-          logger.info(s"[DELETE] /decks/$deckId")
-          handleRequest(() => deckService.deleteDeck(deckId))
+        ,
+        path("decks" / IntNumber) { deckId =>
+          handleRejections(RejectionHandler.default) {
+            put {
+              entity(as[PartialDeckInput]) { deck =>
+                logger.info(s"[PUT] /decks/$deckId")
+                handleRequest(() => deckService.updateDeck(deckId, deck))
+              }
+            }
+          }
         }
-      }
-    )
+        ,
+        path("decks" / IntNumber) { deckId =>
+          handleRejections(RejectionHandler.default) {
+            delete {
+              logger.info(s"[DELETE] /decks/$deckId")
+              complete(StatusCodes.NoContent, deckService.deleteDeck(deckId))
+            }
+          }
+        }
+      )
+    }
   }
 
 }
