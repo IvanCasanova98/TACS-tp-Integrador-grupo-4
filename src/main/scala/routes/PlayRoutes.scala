f286b55663dbebc2324c8c95a1055b34d91c90d0
JulietaAbuin <julieta.abuin@despegar.com>
2021-05-21 00:13:40 -0300

    removed logger in routes

--- src/main/scala/routes/PlayRoutes.scala
+++ src/main/scala/routes/PlayRoutes.scala
@@ -11,13 +11,11 @@ object PlayRoutes {
   def apply(connectedPlayersService: ConnectedPlayersService, matchRooms: MatchRooms): Route = {
     concat(
       path("home") {
-        logger.info("Player joined home")
         parameter("userId") { userId =>
           handleWebSocketMessages(connectedPlayersService.websocketFlow(userId))
         }
       } ~ path("join-match" / IntNumber) { matchId =>
         parameter("userId") { userId =>
-          logger.info(s"Player joined match $matchId")
           handleWebSocketMessages(matchRooms.findOrCreate(matchId).websocketFlow(userId))
         }
       }
