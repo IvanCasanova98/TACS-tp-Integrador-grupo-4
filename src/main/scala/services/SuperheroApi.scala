d489b0d09dc3158faa6e9a6bc16ef7ec29b1e1f9
JulietaAbuin <julieta.abuin@despegar.com>
2021-05-24 21:09:19 -0300

    fixed typo

--- src/main/scala/services/SuperheroApi.scala
+++ src/main/scala/services/SuperheroApi.scala
@@ -8,16 +8,13 @@ import org.apache.http.client.HttpClient
 import org.apache.http.impl.client.HttpClientBuilder
 import scala.util.parsing.json.JSON
 
-
-
-
 case class SuperheroApi() {
   val httpClient: HttpClient = HttpClientBuilder.create.build
 
   val uri = "https://superheroapi.com/api/"
   val access_token = "103706338543731"
 
-  case class AtributeNameClass(name_new: String) extends AttributeName {
+  case class AttributeNameClass(name_new: String) extends AttributeName {
     override def name(): String = name_new
   }
 
@@ -34,21 +31,21 @@ case class SuperheroApi() {
     val responseget = httpClient.execute(get)
     val handler = new BasicResponseHandler
     val json = JSON.parseFull(handler.handleResponse(responseget)).get.asInstanceOf[Map[Any, Any]]("results").asInstanceOf[List[Map[Any, Any]]]
-    json.filter(json_has_all_atributes).map(adapt_card_json)
+    json.filter(json_has_all_attributes).map(adapt_card_json)
 
   }
 
   def adapt_card_json(card_json: Map[Any, Any]): Card = {
-    if (json_has_all_atributes(card_json)) {
+    if (json_has_all_attributes(card_json)) {
       val id: Int = card_json("id").asInstanceOf[String].toInt
       val name: String = card_json("name").asInstanceOf[String]
       val imageUrl: String = card_json("image").asInstanceOf[Map[Any, Any]]("url").asInstanceOf[String]
       val powerStats: Map[Any, Any] = card_json("powerstats").asInstanceOf[Map[Any, Any]]
       if (!json_has_all_powerstats(powerStats,card_json("appearance").asInstanceOf[Map[Any,Any]])){throw NotEnoughAttribute()}
-      var powerStatsCorrect: List[Attribute] = powerStats.map(power => Attribute(AtributeNameClass(power.asInstanceOf[(String, String)]._1), if (power.asInstanceOf[(String, String)]._2 == "null"){0}else{power.asInstanceOf[(String, String)]._2.toInt})).toList
+      var powerStatsCorrect: List[Attribute] = powerStats.map(power => Attribute(AttributeNameClass(power.asInstanceOf[(String, String)]._1), if (power.asInstanceOf[(String, String)]._2 == "null"){0}else{power.asInstanceOf[(String, String)]._2.toInt})).toList
       val height: String = card_json("appearance").asInstanceOf[Map[Any,Any]]("height").asInstanceOf[List[String]](1).replace(" cm", "")
       val weight: String = card_json("appearance").asInstanceOf[Map[Any,Any]]("weight").asInstanceOf[List[String]](1).replace(" kg", "")
-      powerStatsCorrect = powerStatsCorrect ++ List(Attribute(AtributeNameClass("height"), height.toInt), Attribute(AtributeNameClass("weight"), weight.toInt))
+      powerStatsCorrect = powerStatsCorrect ++ List(Attribute(AttributeNameClass("height"), height.toInt), Attribute(AttributeNameClass("weight"), weight.toInt))
       Card(id, name, powerStatsCorrect, imageUrl)}
     else
       {
@@ -62,7 +59,7 @@ case class SuperheroApi() {
       }
     }
 
-  def json_has_all_atributes(card_json: Map[Any, Any]): Boolean = {
+  def json_has_all_attributes(card_json: Map[Any, Any]): Boolean = {
     Set("id", "name", "powerstats", "image").subsetOf(card_json.keys.asInstanceOf[Set[String]])
     }
   def json_has_all_powerstats(powerstarts: Map[Any, Any], appearance: Map[Any, Any]): Boolean = {
--- src/test/scala/servicesTest/SuperheroApiTest.scala
+++ src/test/scala/servicesTest/SuperheroApiTest.scala
@@ -1,4 +1,5 @@
 package servicesTest
 
-class SuperheroApiTest{
+class SuperheroApiTest {
+
 }
